6d848e902fee2b52e922e94a099d606c2664778a
john.westerman@gmail.com <john.westerman@gmail.com>
2018-04-29 17:56:20 -0500

    Commit listOfSquares

--- PythonScripts/listOfSquares.py
+++ PythonScripts/listOfSquares.py
@@ -1,6 +1,15 @@
 squares = []
 for x in range(10):
-    print(squares.append(x**2))
+    print(x**2)
+    squares.append(x**2)
 
 print(squares)
 
+
+# or lambda
+squares = list(map(lambda x: x**2, range(10)))
+print(squares)
+
+# or more simply
+squares = [x**2 for x in range(10)]
+print(squares)
