a0dab363bc68cebad55fe1a8fb0ecb93054f476d
john.westerman@gmail.com <john.westerman@gmail.com>
2018-04-29 09:02:51 -0500

    Test

---
+++ PythonScripts/listMethodExamples.py
@@ -0,0 +1,13 @@
+
+# Examples of list functions
+breeds = ['dachshund','doberman','schnauser','german shepard','dachshund']
+
+print('Count of dachsunds: ', breeds.count('dachshund'))
+
+print('List of breeds: ', breeds)
+
+idx = input('Enter breed index to fetch: ')
+print('Breed at ', breeds.index(idx))
+
+print('List of breeds in reverse ', breeds.reverse)
+
